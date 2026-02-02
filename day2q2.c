9926bc80961f9fa2c003d26f41f48a887f73f733
Kashvi Srivastava <kashvisri2007@gmail.com>
2026-02-03 01:18:08 +0530

    Add files via upload

---
+++ day2q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int n = sizeof(prices) / sizeof(prices[0]);
+
+    printf("%d\n", maxProfit(prices, n));
+
+    return 0;
+}
