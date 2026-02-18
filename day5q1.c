0f7d2b35c29d6d7f8f7b39eed610d93fb95ef5b0
Kashvi Srivastava <kashvisri2007@gmail.com>
2026-02-19 01:30:32 +0530

    Add files via upload

---
+++ day5q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int p, q;
+
+    scanf("%d", &p);
+    int arr1[p];
+    for(int i = 0; i < p; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    scanf("%d", &q);
+    int arr2[q];
+    for(int i = 0; i < q; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    int i = 0, j = 0;
+
+    // Merge both arrays
+    while(i < p && j < q) {
+        if(arr1[i] <= arr2[j]) {
+            printf("%d ", arr1[i]);
+            i++;
+        } else {
+            printf("%d ", arr2[j]);
+            j++;
+        }
+    }
+
+    // Print remaining
