9995bf969d0dcc0c4f43d54c351088cc91988042
Kashvi Srivastava <kashvisri2007@gmail.com>
2026-03-24 17:27:36 +0530

    Add files via upload

---
+++ day25.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert node at the end
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Function to count occurrences of key
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == key)
+            count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, x, key;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        head = insertEnd(head, x);
+    }
+
+    scanf("%d", &key);
+
+    int result = countOccurrences(head, key);
+    printf("%d", result);
+
+    return 0;
+}
