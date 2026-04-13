a00cd42468cc4573252c8043b85fdb95880dfc95
Kashvi Srivastava <kashvisri2007@gmail.com>
2026-04-14 01:52:53 +0530

    Add files via upload

---
+++ day35.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct node *front = NULL, *rear = NULL;
+
+// Enqueue function
+void enqueue(int value) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = value;
+    temp->next = NULL;
+
+    if(rear == NULL) {
+        front = rear = temp;
+    } 
+    else {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+// Display queue
+void display() {
+    struct node* temp = front;
+
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, x;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
